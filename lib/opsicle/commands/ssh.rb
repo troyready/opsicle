fc1012a004a769d457ff3600d1902e51926f6b3e
matthewrkrieger <matt.krieger@sportngin.com>
2015-12-14 14:04:57 -0600

    refactor and add tests

--- lib/opsicle/commands/ssh.rb
+++ lib/opsicle/commands/ssh.rb
@@ -43,18 +43,23 @@ module Opsicle
       @user_profile.ssh_username
     end
 
+    def ssh_ip(instance)
+      if client.config.opsworks_config[:internal_ssh_only]
+        Output.say "This stack requires a private connection, only using internal IPs."
+        instance[:private_ip]
+      else
+        instance[:elastic_ip] || instance[:public_ip]
+      end
+    end
+
     def ssh_command(instance, options={})
       ssh_command = " \"#{options[:"ssh-cmd"].gsub(/'/){ %q(\') }}\"" if options[:"ssh-cmd"] #escape single quotes
       ssh_options = options[:"ssh-opts"] ? "#{options[:"ssh-opts"]} " : ""
-      if client.config.opsworks_config[:vpn_required]
-        ssh_string = "#{ssh_username}@#{instance[:private_ip]}"
+      if instance_ip = ssh_ip(instance)
+        ssh_string = "#{ssh_username}@#{instance_ip}"
       else
-        if instance_ip = instance[:elastic_ip] || instance[:public_ip]
-          ssh_string = "#{ssh_username}@#{instance_ip}"
-        else
-          ssh_string = "#{ssh_username}@#{public_ips.sample} ssh #{instance[:private_ip]}"
-          ssh_options.concat('-A -t ')
-        end
+        ssh_string = "#{ssh_username}@#{public_ips.sample} ssh #{instance[:private_ip]}"
+        ssh_options.concat('-A -t ')
       end
 
       "ssh #{ssh_options}#{ssh_string}#{ssh_command}"
