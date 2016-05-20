dc461f663c8028f7fd76774003f03e45e569dfef
Emma Sax <emma.sax4@gmail.com>
2016-05-20 09:10:20 -0500

    The actual file shouldn't use example files

--- lib/opsicle/credential_converter_helper.rb
+++ lib/opsicle/credential_converter_helper.rb
@@ -4,13 +4,13 @@ module Opsicle
   module CredentialConverterHelper
     def convert_fog_to_aws
       # open/make new credentials file, read, and gather the groups of aws credentials already in file
-      cred_path = File.expand_path("~/.aws/cred_example")
+      cred_path = File.expand_path("~/.aws/credentials")
       cred_file = File.open(cred_path, "a+")
       cred_text = cred_file.read
       cred_groups = cred_text.scan(/\[([a-z]*)\]/).flatten
 
       # open existing fog file, and load as yaml hash
-      fog_path = File.expand_path("~/.fog_example")
+      fog_path = File.expand_path("~/.fog")
       fog_hash = YAML::load_file(fog_path)
 
       # for each environment in the fog file, go through and if it isn't in credentials file, then put it and data in
