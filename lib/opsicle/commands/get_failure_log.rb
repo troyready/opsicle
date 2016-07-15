4b01e05cf10d1a46a8f2a5f60a201f570b37772b
Emma Sax <emma.sax4@gmail.com>
2016-07-15 10:15:21 -0500

    Save point

--- lib/opsicle/commands/get_failure_log.rb
+++ lib/opsicle/commands/get_failure_log.rb
@@ -1,21 +1,20 @@
-# require "opsicle/deploy_helper"
+require "opsicle/get_failure_log_helper"
 
 module Opsicle
   class GetFailureLog
     include GetFailureLogHelper
-    attr_reader :client
+    attr_reader :client, :stack
 
     def initialize(environment)
       @environment = environment
       @client = Client.new(environment)
+      @stack = Opsicle::Stack.new(@client)
     end
 
-    def execute(options={ monitor: true })
-      Output.say "Getting most recent failure log..."
+    def execute
+      puts "Getting most recent failure log..."
 
-      get_failure_log
-
-      # do something here
+      get_recent_failure_log(@client, @stack)
     end
   end
 end
--- lib/opsicle/get_failure_log_helper.rb
+++ lib/opsicle/get_failure_log_helper.rb
@@ -2,8 +2,10 @@
 
 module Opsicle
   module GetFailureLogHelper
-    def get_failure_log
-      # do something good here
+    def get_recent_failure_log(client, stack)
+      stack_id = {stack_id: stack.stack_id}
+      # client.describe_deployments(stack_id).deployments.select{ |d| !d.status.eql? "successful" }.first.deployment_id
+      # system("open", client.describe_commands(deployment_id: deploy_id).commands[0].log_url)
     end
   end
 end
