9eec5f7864ac21270a0b7cbd181fadf8b3afc089
Emma Sax <emma.sax4@gmail.com>
2016-07-15 13:20:52 -0500

    Making tests for s3 buckets pass

--- lib/opsicle/s3_bucket.rb
+++ lib/opsicle/s3_bucket.rb
@@ -2,17 +2,17 @@ require 'pathname'
 
 module Opsicle
   class S3Bucket
-     attr_reader :bucket
+    attr_reader :bucket
 
-     def initialize(client, bucket_name)
-        @bucket = Aws::S3::Bucket.new(name: bucket_name, client: client.s3)
-        raise UnknownBucket unless @bucket.exists?
-     end
+    def initialize(client, bucket_name)
+      @bucket = Aws::S3::Bucket.new(name: bucket_name, client: client.s3)
+      raise UnknownBucket unless @bucket.exists?
+    end
 
-     def update(object)
+    def update(object)
       obj = bucket.object(object)
       obj.upload_file(Pathname.new(object))
-     end
+    end
   end
 
   UnknownBucket = Class.new(StandardError)
