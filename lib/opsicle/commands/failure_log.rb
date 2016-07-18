1334ca3b042f32f35a63ce7ec51f8edcd75720ad
Emma Sax <emma.sax4@gmail.com>
2016-07-18 11:49:54 -0500

    Refactoring

--- lib/opsicle/commands/failure_log.rb
+++ lib/opsicle/commands/failure_log.rb
@@ -15,31 +15,32 @@ module Opsicle
     end
 
     def fetch
-      failed_deployment_id, failed_deployments_instances = fetch_deployments
-      involved_instance_id = fetch_instances(failed_deployments_instances)
-      log_url = fetch_commands(involved_instance_id, failed_deployment_id)
-      system("open", log_url) if log_url
-    end
-
-    def fetch_deployments
-      deployments = @client.opsworks.describe_deployments(stack_id: @stack.stack_id).deployments
-      failed_deployments = deployments.select{ |deploy| !deploy.status.eql? "successful" }
+      failed_deployments = fetch_failed_deployments
       failed_deployment_id = failed_deployments.first.deployment_id
       failed_deployments_instances = failed_deployments.first.instance_ids
 
-      return failed_deployment_id, failed_deployments_instances
+      involved_instance_id = fetch_instance_id(failed_deployments_instances)
+
+      target_failed_command = fetch_target_command(involved_instance_id, failed_deployment_id)
+      log_url = target_failed_command.first.log_url
+      
+      system("open", log_url) if log_url
     end
 
-    def fetch_instances(failed_deployments_instances)
+    def fetch_failed_deployments
+      deployments = @client.opsworks.describe_deployments(stack_id: @stack.stack_id).deployments
+      return deployments.select{ |deploy| !deploy.status.eql? "successful" }
+    end
+
+    def fetch_instance_id(failed_deployments_instances)
       involved_instances = @client.opsworks.describe_instances(instance_ids: failed_deployments_instances).instances
       choice = select_instance(involved_instances)
       return involved_instances[choice-1].instance_id
     end
 
-    def fetch_commands(involved_instance_id, failed_deployment_id)
+    def fetch_target_command(involved_instance_id, failed_deployment_id)
       command_list = @client.opsworks.describe_commands(instance_id: involved_instance_id)[:commands]
-      target_failed_command = command_list.select{ |command| command.deployment_id == failed_deployment_id }
-      return target_failed_command.first.log_url
+      return command_list.select{ |command| command.deployment_id == failed_deployment_id }
     end
 
     def select_instance(instance_list)
