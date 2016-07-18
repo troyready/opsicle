054fa5c98175be80ac2e37110217b2c2f4e31a70
Emma Sax <emma.sax4@gmail.com>
2016-07-18 11:42:03 -0500

    Ask user for which instance they want; refactoring; fixing tests

--- lib/opsicle/commands/failure_log.rb
+++ lib/opsicle/commands/failure_log.rb
@@ -15,14 +15,44 @@ module Opsicle
     end
 
     def fetch
-      stack_id_hash = {stack_id: @stack.stack_id}
-      deployments = @client.opsworks.describe_deployments(stack_id_hash).deployments
+      failed_deployment_id, failed_deployments_instances = fetch_deployments
+      involved_instance_id = fetch_instances(failed_deployments_instances)
+      log_url = fetch_commands(involved_instance_id, failed_deployment_id)
+      system("open", log_url) if log_url
+    end
+
+    def fetch_deployments
+      deployments = @client.opsworks.describe_deployments(stack_id: @stack.stack_id).deployments
       failed_deployments = deployments.select{ |deploy| !deploy.status.eql? "successful" }
       failed_deployment_id = failed_deployments.first.deployment_id
-      command_list = @client.opsworks.describe_commands(deployment_id: failed_deployment_id)
-      log_url = command_list[:commands].first.log_url
+      failed_deployments_instances = failed_deployments.first.instance_ids
 
-      system("open", log_url)
+      return failed_deployment_id, failed_deployments_instances
+    end
+
+    def fetch_instances(failed_deployments_instances)
+      involved_instances = @client.opsworks.describe_instances(instance_ids: failed_deployments_instances).instances
+      choice = select_instance(involved_instances)
+      return involved_instances[choice-1].instance_id
+    end
+
+    def fetch_commands(involved_instance_id, failed_deployment_id)
+      command_list = @client.opsworks.describe_commands(instance_id: involved_instance_id)[:commands]
+      target_failed_command = command_list.select{ |command| command.deployment_id == failed_deployment_id }
+      return target_failed_command.first.log_url
+    end
+
+    def select_instance(instance_list)
+      if instance_list.length == 1
+        choice = 1
+      else
+        Output.say "Choose an Opsworks instance:"
+        instance_list.each_with_index do |instance, index|
+          Output.say "#{index+1}) #{instance[:hostname]}"
+        end
+        choice = Output.ask("? ", Integer) { |q| q.in = 1..instance_list.length }
+      end
+      return choice
     end
   end
 end
--- spec/opsicle/commands/failure_log_spec.rb
+++ spec/opsicle/commands/failure_log_spec.rb
@@ -4,14 +4,16 @@ require "opsicle"
 module Opsicle
   describe FailureLog do
     before do
-      deploy1 = double('deploy', status: "fail", deployment_id: '678903')
-      deploy2 = double('deploy', status: "fail", deployment_id: '294172')
+      instance = double('instance', instance_id: 123456)
+      instances = double('instances', instances: [instance])
+      deploy1 = double('deploy', status: "fail", deployment_id: '678903', instance_ids: [123456])
+      deploy2 = double('deploy', status: "fail", deployment_id: '294172', instance_ids: [123456])
       deployments = double('deployments', deployments: [deploy1, deploy2])
-      command1 = double('command', log_url: 'http://example1.com')
-      command2 = double('command', log_url: 'http://example2.com')
+      command1 = double('command', log_url: 'http://example1.com', deployment_id: '678903')
+      command2 = double('command', log_url: 'http://example2.com', deployment_id: '294172')
       commands = {:commands => [command1, command2]}
       @stack = double('stack', stack_id: '12345')
-      opsworks_client = double('opsworks_client', describe_deployments: deployments, describe_commands: commands)
+      opsworks_client = double('opsworks_client', describe_deployments: deployments, describe_instances: instances,describe_commands: commands)
       @client = double('client', opsworks: opsworks_client)
       allow(Client).to receive(:new).and_return(@client)
       allow(Opsicle::Stack).to receive(:new).and_return(@stack)
