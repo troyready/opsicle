b57c6fa18da65a3b662f9a4f08f26bba3e1ce7eb
Emma Sax <emma.sax4@gmail.com>
2016-08-29 09:37:27 -0500

    Refactoring into separate methods and auto increment until unique hostname

--- lib/opsicle/commands/clone_instance.rb
+++ lib/opsicle/commands/clone_instance.rb
@@ -13,52 +13,90 @@ module Opsicle
 
     def execute(options={})
       puts "Stack ID = #{@stack_id}"
-      puts "\nLayers:\n"
-      layers = @opsworks.describe_layers({ :stack_id => @stack_id }).data
-      layers[:layers].each_with_index {|layer, index| puts "#{index.to_i + 1} #{layer.name}"}
-      layer_index = @cli.ask("Layer?\n", Integer) { |q| q.in = 1..layers[:layers].length.to_i } - 1
+      layer_id = select_layer
+      all_instances = @opsworks.describe_instances({:layer_id => layer_id}).data
+      all_hostnames = all_instances[:instances].collect { |instance| instance.hostname }
+      instance_indices_list = select_instances(layer_id)
 
-      layer_name = layers.layers[layer_index].name
-      layer_id = layers.layers[layer_index].layer_id
-
-      puts "\nInstances:\n"
-      instances = @opsworks.describe_instances({:layer_id => layer_id}).data
-      instances[:instances].each_with_index {|instance, index| puts "#{index.to_i + 1} #{instance.status} #{instance.hostname}" }
-      instance_indexes_list = @cli.ask("Instances? (comma sep list)\n", lambda { |str| str.split(/,\s*/) })
-      instance_indexes_list.map! { |instance_index| instance_index.to_i - 1 }
-
-      hostname_modifier = @cli.ask("\nHostname modifier? (number from 1 to 90\n", Integer) { |q| q.in = 1..90 }
-
-      instance_indexes_list.each do |instance_index|
-        old_instance = instances.instances[instance_index]
-        # Overrides
-        new_instance_hostname = instances.instances[instance_index].hostname.gsub(/(\d\d\z)/) { "#{($1.to_i + hostname_modifier).to_s.rjust(2, '0')}"}
-        options[:ami]? new_instance_ami_id = options[:ami] : new_instance_ami_id = old_instance.ami_id
-        options[:instance_type]? instance_type = options[:instance_type] : instance_type = old_instance.instance_type 
-        puts "\nGenerated hostname = #{new_instance_hostname}\n"
-
-        new_instance = @opsworks.create_instance({
-          stack_id: old_instance.stack_id, # required
-          layer_ids: old_instance.layer_ids, # required
-          instance_type: instance_type, # required
-          auto_scaling_type: old_instance.auto_scaling_type, # accepts load, timer
-          hostname: new_instance_hostname,
-          os: old_instance.os,
-          ami_id: new_instance_ami_id,
-          ssh_key_name: old_instance.ssh_key_name,
-          availability_zone: old_instance.availability_zone,
-          virtualization_type: old_instance.virtualization_type,
-          subnet_id: old_instance.subnet_id,
-          architecture: old_instance.architecture, # accepts x86_64, i386
-          root_device_type: old_instance.root_device_type, # accepts ebs, instance-store
-          install_updates_on_boot: old_instance.install_updates_on_boot,
-          #ebs_optimized: old_instance.ebs_optimized,
-          agent_version: old_instance.agent_version,
-          tenancy: old_instance.tenancy,
-        })
-        puts "New instance is created: #{new_instance.instance_id}"
+      instance_indices_list.each do |instance_index|
+        clone_instance(instance_index)
       end
 
+    end
+
+    def select_layer
+      puts "\nLayers:\n"
+      layers = @opsworks.describe_layers({ :stack_id => @stack_id }).data
+      layers[:layers].each_with_index {|layer, index| puts "#{index.to_i + 1}) #{layer.name}"}
+      layer_index = @cli.ask("Layer?\n", Integer) { |q| q.in = 1..layers[:layers].length.to_i } - 1
+      layers.layers[layer_index].layer_id
+    end
+
+    def select_instances(layer_id)
+      puts "\nInstances:\n"
+      instances = @opsworks.describe_instances({ :layer_id => layer_id }).data
+      instances[:instances].each_with_index {|instance, index| puts "#{index.to_i + 1}) #{instance.status} - #{instance.hostname}" }
+      instance_indices_list = @cli.ask("Instances? (enter as a comma separated list)\n", lambda { |str| str.split(/,\s*/) })
+      instance_indices_list.map! { |instance_index| instance_index.to_i - 1 }
+    end
+
+    def clone_instance(instance_index)
+      old_instance = all_instances.instances[instance_index]
+
+      if old_instance.hostname =~ /\d/
+        new_instance_hostname = increment_hostname(old_instance.hostname, all_hostnames)
+      else
+        new_instance_hostname = old_instance.hostname << "_clone"
+      end
+        
+      puts "\nAutomatically generated hostname: #{new_instance_hostname}\n"
+
+      rewriting = @cli.ask("Do you wish to rewrite this hostname?\n1) Yes\n2) No", Integer)
+      if rewriting == 1
+        new_instance_hostname = @cli.ask("Please write in the new instance's hostname and press ENTER:")
+      end
+
+      puts "\nWe will make a new instance with hostname: #{new_instance_hostname}"
+
+      options[:ami] ? ami_id = options[:ami] : ami_id = old_instance.ami_id
+      options[:instance_type] ? instance_type = options[:instance_type] : instance_type = old_instance.instance_type
+      options[:agent_version] ? agent_version = options[:agent_version] : agent_version = old_instance.agent_version
+
+      create_new_instance(old_instance, instance_type, new_instance_hostname, ami_id, agent_version)
+    end
+
+    def increment_hostname(hostname, all_hostnames)
+      until hostname_is_unique(hostname, all_hostnames) do
+        hostname = hostname.gsub(/(\d\d\z)/) { "#{($1.to_i + 1).to_s.rjust(2, '0')}"}
+      end
+      hostname
+    end
+
+    def hostname_is_unique(hostname, all_hostnames)
+      !all_hostnames.include?(hostname)
+    end
+
+    def create_new_instance(old_instance, instance_type, new_instance_hostname, ami_id, agent_version)
+      new_instance = @opsworks.create_instance({
+        stack_id: old_instance.stack_id, # required
+        layer_ids: old_instance.layer_ids, # required
+        instance_type: instance_type, # required
+        auto_scaling_type: old_instance.auto_scaling_type, # accepts load, timer
+        hostname: new_instance_hostname,
+        os: old_instance.os,
+        ami_id: ami_id,
+        ssh_key_name: old_instance.ssh_key_name,
+        availability_zone: old_instance.availability_zone,
+        virtualization_type: old_instance.virtualization_type,
+        subnet_id: old_instance.subnet_id,
+        architecture: old_instance.architecture, # accepts x86_64, i386
+        root_device_type: old_instance.root_device_type, # accepts ebs, instance-store
+        install_updates_on_boot: old_instance.install_updates_on_boot,
+        #ebs_optimized: old_instance.ebs_optimized,
+        agent_version: agent_version,
+        tenancy: old_instance.tenancy,
+      })
+      puts "New instance is created: #{new_instance.instance_id}"
     end
   end
 end
