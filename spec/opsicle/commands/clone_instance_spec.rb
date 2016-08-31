b4d6bdce92d2332c38c1295fad8d8343170c621d
Emma Sax <emma.sax4@gmail.com>
2016-08-31 10:24:24 -0500

    Adding more tests

--- spec/opsicle/commands/clone_instance_spec.rb
+++ spec/opsicle/commands/clone_instance_spec.rb
@@ -40,7 +40,7 @@ module Opsicle
                                  :opsworks => @opsworks)
         
       allow(Client).to receive(:new).with(:environment).and_return(@client)
-      
+
       allow_any_instance_of(HighLine).to receive(:ask).with("Layer?\n", Integer).and_return(2)
       allow_any_instance_of(HighLine).to receive(:ask).with("Instances? (enter as a comma separated list)\n", String).and_return('2')
       allow_any_instance_of(HighLine).to receive(:ask).with("Do you wish to rewrite this hostname?\n1) Yes\n2) No", Integer).and_return(2)
@@ -68,6 +68,86 @@ module Opsicle
       it "should list layers" do
         expect(@opsworks).to receive(:describe_layers)
         CloneInstance.new(:environment).select_layer
+      end
+
+      it "should get the layer id" do
+        expect(@layer2).to receive(:layer_id)
+        CloneInstance.new(:environment).select_layer
+      end
+    end
+
+    context "#select_instances" do
+      it "should list instances" do
+        expect(@instances).to receive(:instances)
+        CloneInstance.new(:environment).select_instances(@instances)
+      end
+
+      it "should get the hostnames and statuses" do
+        expect(@instance1).to receive(:hostname)
+        expect(@instance1).to receive(:status)
+        expect(@instance2).to receive(:hostname)
+        expect(@instance2).to receive(:status)
+        CloneInstance.new(:environment).select_instances(@instances)
+      end
+    end
+
+    context "#make_new_hostname" do
+      it "should list instances" do
+        clone = CloneInstance.new(:environment)
+        expect(clone).to receive(:increment_hostname).and_return('example-hostname-03')
+        clone.make_new_hostname('example-hostname-01', ['example-hostname-01', 'example-hostname-02'])
+      end
+    end
+
+    context "#increment_hostname" do
+      it "should increment the hostname" do
+        clone = CloneInstance.new(:environment)
+        expect(clone).to receive(:hostname_unique?).and_return(true)
+        clone.increment_hostname('example-hostname-01', ['example-hostname-01', 'example-hostname-02'])
+      end
+    end
+
+    context "#clone_instance" do
+      it "should grab instances and make new hostname" do
+        clone = CloneInstance.new(:environment)
+        expect(@instances).to receive(:instances)
+        expect(clone).to receive(:make_new_hostname).and_return('example-hostname-03')
+        clone.clone_instance(@instances, ['example-hostname-01', 'example-hostname-02'], 1, {})
+      end
+
+      it "should get information from old instance" do
+        clone = CloneInstance.new(:environment)
+        expect(@instance2).to receive(:ami_id)
+        expect(@instance2).to receive(:instance_type)
+        expect(@instance2).to receive(:agent_version)
+        clone.clone_instance(@instances, ['example-hostname-01', 'example-hostname-02'], 1, {})
+      end
+
+      it "should create new instance" do
+        clone = CloneInstance.new(:environment)
+        expect(clone).to receive(:create_new_instance).and_return(true)
+        clone.clone_instance(@instances, ['example-hostname-01', 'example-hostname-02'], 1, {})
+      end
+    end
+
+    context "#create_new_instance" do
+      it "should create an instance" do
+        clone = CloneInstance.new(:environment)
+        expect(@opsworks).to receive(:create_instance).and_return(@new_instance)
+        clone.create_new_instance(@instance2, 'instance_type', 'hostname', 'ami_id', 'agent_version')
+      end
+
+      it "should take information from old instance" do
+        clone = CloneInstance.new(:environment)
+        expect(@instance2).to receive(:stack_id)
+        expect(@instance2).to receive(:layer_ids)
+        expect(@instance2).to receive(:auto_scaling_type)
+        expect(@instance2).to receive(:os)
+        expect(@instance2).to receive(:ssh_key_name)
+        expect(@instance2).to receive(:availability_zone)
+        expect(@instance2).to receive(:virtualization_type)
+        expect(@instance2).to receive(:subnet_id)
+        clone.create_new_instance(@instance2, 'instance_type', 'hostname', 'ami_id', 'agent_version')
       end
     end
 
